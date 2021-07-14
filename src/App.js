1c0a37016ab336593e7adbcba665b40d8a001cde
Caio Oliveira <caio.goncalves.pro@gmail.com>
2021-07-14 18:25:59 -0300

    curso ciclo de vida components

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeCategorias from "./components/ListaDeCategorias";
+import "./assets/App.css";
+import "./assets/index.css";
+import Categorias from "./dados/categorias";
+import Notas from "./dados/notas";
+class App extends Component {
+  constructor() {
+    super();
+    this.categorias = new Categorias();
+    this.notas = new Notas();
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro
+          categorias={this.categorias}
+          criarNota={this.notas.adicionarNota.bind(this.notas)}
+        />
+        <main className="conteudo-principal">
+          <ListaDeCategorias
+            adicionarCategoria={this.categorias.adicionarCategoria.bind(
+              this.categorias
+            )}
+            categorias={this.categorias}
+          />
+          <ListaDeNotas
+            apagarNota={this.notas.apagarNota.bind(this.notas)}
+            notas={this.notas}
+          />
+        </main>
+      </section>
+    );
+  }
+}
+export default App;
